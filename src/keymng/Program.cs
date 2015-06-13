ed6ff5dba931be5fd409ae0e9a401c23f871639a
Kaelan Fouwels <kfouwels@outlook.com>
2015-06-13 13:44:05 +0100

    initial

---
+++ src/keymng/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace keymng
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+			XmlDocument itemDoc = new XmlDocument();
+			itemDoc.Load(@"KeysExport.xml");
+
+		}
+    }
+}
