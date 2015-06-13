2e23cd01c4405fe7684c30f1e22a27c2a8ade7e5
Kaelan Fouwels <kfouwels@outlook.com>
2015-06-13 14:23:32 +0100

    Parsing to object

--- src/keymng/Program.cs
+++ src/keymng/Program.cs
@@ -1,20 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
 
 namespace keymng
 {
-    public class Program
-    {
-        public void Main(string[] args)
-        {
-			XmlDocument itemDoc = new XmlDocument();
-			itemDoc.LoadXml(@"KeysExport.xml");
+	public class Program
+	{
+		public void Main(string[] args)
+		{
+			var xml = File.ReadAllText(@"KeysExport.xml"); //yahyah, streamreading is faster, I know
 
+			var xmldoc = new XmlDocument();
+			xmldoc.LoadXml(xml);
 
+			var obj = new Models.KeyCollection();
+			obj.Product_Keys = new List<Models.Product_Key>();
 
+			foreach (var node in xmldoc.DocumentElement.ChildNodes)
+			{
+				var x = (XmlElement)node;
+				var keymodel = new Models.Product_Key
+				{
+					Name = x.Attributes["Name"].InnerText,
+					Keys = new List<Models.Key>()
+				};
+
+				foreach (var key in x.ChildNodes)
+				{
+					var y = (XmlElement)key;
+					keymodel.Keys.Add(new Models.Key
+					{
+						ID = y.Attributes["ID"].InnerText,
+						Type = y.Attributes["Type"].InnerText,
+						ClaimedDate = y.Attributes["ClaimedDate"].InnerText,
+						Value = y.InnerText
+					});
+
+				}
+				obj.Product_Keys.Add(keymodel);
+
+			}
 		}
-    }
+	}
 }
